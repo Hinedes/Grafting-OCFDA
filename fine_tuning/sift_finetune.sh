8271fde587e9e728aeee0080693dd2aef73d58e3
fzmushko <zmushko.ph.a@gmail.com>
2025-02-18 13:38:02 +0000

    wandb logging of accuracy

--- fine_tuning/sift_finetune.sh
+++ fine_tuning/sift_finetune.sh
@@ -1,19 +1,27 @@
-mkdir ./result/model_lora -p
+export OMP_NUM_THREADS=8
 
-CUDA_VISIBLE_DEVICES=0 python commonsense_evaluate.py \
-    --model LLaMA-7B \
-    --adapter super \
-    --dataset boolq \
-    --batch_size 1 \
-    --base_model 'meta-llama/Llama-3.2-1B' \
-    --sparse_rate 0.011363636363636364 \
-    --lora_weights './trained_models/llama-sift' | tee -a './result/model_lora/boolq.txt'
+export WANDB_API_KEY=$(cat /slot/sandbox/d/secret/*)
 
-CUDA_VISIBLE_DEVICES=0 python commonsense_evaluate.py \
-    --model LLaMA-7B \
-    --adapter super \
-    --dataset piqa \
-    --batch_size 1 \
-    --base_model 'meta-llama/Llama-3.2-1B' \
-    --sparse_rate 0.011363636363636364 \
-    --lora_weights './trained_models/llama-sift' | tee -a './result/model_lora/piqa.txt'
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export CUDA_VISIBLE_DEVICES=2
+# WORLD_SIZE=2 torchrun --nproc_per_node=2 --master_port=3192 finetune.py \
+python finetune.py \
+  --base_model 'unsloth/Llama-3.2-1B' \
+  --data_path 'commonsense_15k.json' \
+  --output_dir './trained_models/llama-super' \
+  --save_step 10 \
+  --eval_step 10 \
+  --batch_size 16 \
+  --micro_batch_size 8 \
+  --num_epochs 3 \
+  --learning_rate 5e-5 \
+  --cutoff_len 256 \
+  --val_set_size 120 \
+  --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
+  --compile 0 \
+  --wandb_project galore_commonsense_1b_TEST \
+  --wandb_run_name TEST_EVAL_TO_WANDB \
+  --sparse_rate 0.01171875 \
+  --adapter_name super \
+  --seed 0 \
+  --max_steps 12 \
